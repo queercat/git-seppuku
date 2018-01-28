b6f8e4c9f12cf7dcbe3ebce6d97ed550c28f2082
Nicolsek <tusek.nicole@gmail.com>
2018-01-27 19:03:18 -0800

    Fixed from pushing to all branches.

--- seppuku.sh
+++ seppuku.sh
@@ -6,4 +6,4 @@ git rm -r *; # Remove all files in working directory.
 git add ./; # Add all the removed files to be staged.
 git status; # Show the status of git.
 git commit -a -m "Seppuku."; # Add the message of seppuku.
-git push -f -a;  # Push the commit to all branches.
+git push -f;  # Push the commit to the branch.
