39cca23ca30f86d422ab9f7fdda3c6859b0d0beb
nicolsek <tusek.nicole@gmail.com>
2018-01-28 01:47:17 -0800

    Fixed syntasx for shell script.

--- seppuku.sh
+++ seppuku.sh
@@ -2,21 +2,20 @@
 
 echo "Seppuku has begun...";
 
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo 'Welcome to seppuku!';
     echo 'Arguments:';
     echo '*  Push to ALL branches. (-a --all).';
     echo '*  View this help document. (-h, --help).';
-fi
-
-git rm -r *; # Remove all files in working directory.
-git add ./; # Add all the removed files to be staged.
-git status; # Show the status of git.
-git commit -a -m "Seppuku."; # Add the message of seppuku.
-
-if [[ "$1" == "-a" || "$1" == "--all" ]]; then
-    git push -f -a; # Push to all branches. 
 else
-    git push -f;  # Push the commit to the branch.
+    git rm -r *; # Remove all files in working directory.
+    git add ./; # Add all the removed files to be staged.
+    git status; # Show the status of git.
+    git commit -a -m "Seppuku."; # Add the message of seppuku.
+    
+    if [ "$1" = "-a" ] || [ "$1" = "--all" ]; then
+        git push -f -a; # Push to all branches. 
+    else
+        git push -f;  # Push the commit to the branch.
+    fi
 fi
-
