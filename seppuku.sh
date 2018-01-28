6b44a489f205d54455073864abf73d85217ae919
nicolsek <tusek.nicole@gmail.com>
2018-01-28 01:06:14 -0800

    Fixed typo leading to bug + linted better.

--- seppuku.sh
+++ seppuku.sh
@@ -3,10 +3,10 @@
 echo "Seppuku has begun...";
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo 'Welcome to seppuku!'
-    echo 'Arguments:'
-    echo '*  Push to ALL branches. (-a --all).'
-    echo '*  View this help document. (-h, --help).'
+    echo 'Welcome to seppuku!';
+    echo 'Arguments:';
+    echo '*  Push to ALL branches. (-a --all).';
+    echo '*  View this help document. (-h, --help).';
 fi
 
 git rm -r *; # Remove all files in working directory.
@@ -14,7 +14,7 @@ git add ./; # Add all the removed files to be staged.
 git status; # Show the status of git.
 git commit -a -m "Seppuku."; # Add the message of seppuku.
 
-if [[ $1 == "-a" || $1 == "--all" ]]; then
+if [[ "$1" == "-a" || "$1" == "--all" ]]; then
     git push -f -a; # Push to all branches. 
 else
     git push -f;  # Push the commit to the branch.
